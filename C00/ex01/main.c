ff6c069991c9eca1242c710fa0c5bc117c15bd07
macaruan <mhp.cua@gmail.com>
2026-04-13 19:26:19 +0200

    holle

---
+++ C00/ex01/main.c
@@ -0,0 +1,10 @@
+#include <avr/io.h>
+
+int
+main (void)
+{
+	DDRB |= (1 << 0); //set register pos 0 to 1 (led)  to set it on output for the led to recieve information
+	PORTB |= (1 << 0); //set pin to turn on the led
+
+	while(1){} // loop
+}
---
+++ C00/ex02/main.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRB |= (1 << 0); //set register pos 0 to 1 (led) to set it on output for the led to recieve information
+	DDRD &= ~(0 << 2); //set register pos 0 to 1 (button) to set it on input
+
+	while (1)
+	{
+		if (!(PIND &= (1 << 2))) // check with an inverted AND so we can check if the button is pressed
+		{
+			PINB |= (1 << 0);
+		}
+		else
+		{
+			PINB |= (0 << 0);
+		}
+	}
+}
