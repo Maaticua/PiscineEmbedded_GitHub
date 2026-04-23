1a5d504edf4f092e1d2abc479f01ddb3891d5a54
macaruan <mhp.cua@gmail.com>
2026-04-23 17:52:27 +0200

    ex00 n 01 finished

--- M05/ex00/main.c
+++ M05/ex00/main.c
@@ -2,7 +2,76 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define BAUD 115200 // baude rate
+#define MYUBRR F_CPU/16/BAUD  // calcul pour synchroniser la board et le pc
+
+void USART_RX_vect(void) __attribute__((signal));  // on declare un vecteur d'interuption et on ajoute des instruction speciale au compilateur, 1er sauvgarder letat et 2e reti p74
+
+void uart_init( unsigned int ubrr)
+{
+	UBRR0H = (unsigned char)(ubrr>>8);  //setup le baudrate high | p185
+	UBRR0L = (unsigned char)ubrr;  // setup le baudrate low      | bauderate separer en deux car cest une valeur 12bits p185
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0); // on active le recepteur RXENO pour recevoir des donnees et le recepteur TXENO pour en emettre p185
+}
+
+void uart_tx(char c)
+{
+	while (!( UCSR0A & (1<<UDRE0))) {} // indicateur pour savoir si le tampon est vide et est pret a recevoir un nouveau character p186
+	UDR0 = c;
+}
+
+char uart_rx(void)
+{
+	while ( !(UCSR0A & (1<<RXC0)) ) // attend de recevoir de la data
+					;
+	return UDR0; // retourne ce qu'il recoit
+}
+
+void USART_RX_vect(void) // remplace ISR
+{
+	char c = UDR0;
+	uart_tx(c);
+}
+
+void uart_printstr(const char* str)
+{
+	uint8_t i = 0;
+	while (str[i])
+		uart_tx(str[i++]); //ecrit ce qu'il recoit char par char
+}
+void	uart_puthexu8nbr(uint8_t n)
+{
+	char* hex = "0123456789ABCDEF";
+
+	if (n >= 16)
+	{
+		uart_puthexu8nbr(n / 16);
+	}
+	uart_tx(hex[n % 16]);
+}
+
 int main(void)
 {
+	uint8_t i = 0;
+	uart_init(MYUBRR);// on initialise L'UART p184
 
+	ADCSRA |= (1 << ADEN) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // on allume l'ADC avec ADEN p258 et on setup l'horloge de l'ADC p258 et https://marcjuneau.ca/?p=112
+	ADMUX |= (1 << ADLAR) | (1 << REFS0); // ADLAR va nous permettre de "pousser" les 8 plus important bits dans le registre ADCH  p257
+
+	while(1)
+	{
+		// potentiometre
+		ADCSRA |= (1 << ADSC); // on active l'ADC Start Conversion p258
+		while (ADCSRA & (1 << ADSC)){} // on attend la fin de la convertion p258
+		i = ADCH;
+		uart_puthexu8nbr(i);
+		uart_printstr("\r\n");
+		_delay_ms(20);
+	}
 }
+
+	// LDR PC1 NTC PC2
+	// MUX3    MUX2    MUX1    MUX0    Pin that will be read
+	// 0        0       0       0        PORTC0    Analogic0 RV1 ADC (potentiometre)
+	// 0        0       0       1        PORTC1    Analogic1 R14 LDR (lumiere)
+	// 0        0       1       0        PORTC2    Analogic2 R20 NTC (temperature)
