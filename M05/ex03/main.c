ab428381d569351095ead3c98313555f94b4f299
macaruan <mhp.cua@gmail.com>
2026-04-24 15:40:56 +0200

    yeee

--- M05/ex03/main.c
+++ M05/ex03/main.c
@@ -2,7 +2,89 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define BAUD 115200 // baude rate
+#define MYUBRR F_CPU/16/BAUD  // calcul pour synchroniser la board et le pc
+
+void USART_RX_vect(void) __attribute__((signal));  // on declare un vecteur d'interuption et on ajoute des instruction speciale au compilateur, 1er sauvgarder letat et 2e reti p74
+
+void uart_init( unsigned int ubrr)
+{
+	UBRR0H = (unsigned char)(ubrr>>8);  //setup le baudrate high | p185
+	UBRR0L = (unsigned char)ubrr;  // setup le baudrate low      | bauderate separer en deux car cest une valeur 12bits p185
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0); // on active le recepteur RXENO pour recevoir des donnees et le recepteur TXENO pour en emettre p185
+}
+
+void uart_tx(char c)
+{
+	while (!( UCSR0A & (1<<UDRE0))) {} // indicateur pour savoir si le tampon est vide et est pret a recevoir un nouveau character p186
+	UDR0 = c;
+}
+
+char uart_rx(void)
+{
+	while ( !(UCSR0A & (1<<RXC0)) ) // attend de recevoir de la data
+					;
+	return UDR0; // retourne ce qu'il recoit
+}
+
+void USART_RX_vect(void) // remplace ISR
+{
+	char c = UDR0;
+	uart_tx(c);
+}
+
+void uart_printstr(const char* str)
+{
+	uint8_t i = 0;
+	while (str[i])
+		uart_tx(str[i++]); //ecrit ce qu'il recoit char par char
+}
+void	uart_puthexu8nbr(uint8_t n)
+{
+	char* hex = "0123456789ABCDEF";
+
+	if (n >= 16)
+	{
+		uart_puthexu8nbr(n / 16);
+	}
+	uart_tx(hex[n % 16]);
+}
+
+void	uart_putdecistr(uint16_t n)
+{
+	char* hex = "0123456789";
+
+	if (n >= 10)
+	{
+		uart_putdecistr(n / 10);
+	}
+	uart_tx(hex[n % 10]);
+}
+
 int main(void)
 {
+	uint16_t i = 0;
+	uart_init(MYUBRR);// on initialise L'UART p184
 
+	ADCSRA |= (1 << ADEN) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // on allume l'ADC avec ADEN p258 et on setup l'horloge de l'ADC p258 et https://marcjuneau.ca/?p=112
+	ADMUX |= (1 << REFS0) | (1 << REFS1) | (1 << MUX3); // on active le internal 1.1 Volatge Reference, obligatoire pour que la mesure de temp fonctionne p256 (le tension de reference) p257 et on active le onchip temperature sensor qui se trouve sur le ADC8
+
+	while(1)
+	{
+		ADCSRA |= (1 << ADSC); // on active l'ADC Start Conversion p258
+		while (ADCSRA & (1 << ADSC)){} // on attend la fin de la convertion p258
+		i = ADC;
+		//	calcul de la pente et de l'offset de la relation lineaire entre voltage et degre a partir
+		//	des 3 points de la courbe donner en exemple ADC = a * T + b
+		//	a = (adc2 - adc1) / (T1 - T2) = 1.22, b = adc - a * T = 324.31 p256 + https://clg-cousteau-mery.ac-versailles.fr/IMG/pdf/3e-Chap5-Cours-Fonctions_lineaires_et_affines.pdf
+		uart_putdecistr(((i - 324) * 100) / 122);
+		uart_printstr("\r\n");
+		_delay_ms(20);
+	}
 }
+
+	// LDR PC1 NTC PC2
+	// MUX3    MUX2    MUX1    MUX0    Pin that will be read
+	// 0        0       0       0        PORTC0    Analogic0 RV1 ADC (potentiometre)
+	// 0        0       0       1        PORTC1    Analogic1 R14 LDR (lumiere)
+	// 0        0       1       0        PORTC2    Analogic2 R20 NTC (temperature)
--- M05/ex04/main.c
+++ M05/ex04/main.c
@@ -2,7 +2,102 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define LEDRGB (1 << PD3) | (1 << PD5) | (1 << PD6) // un define pour faciliter initialisation
+#define LED (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB4)
+
+#define RED (1 << PD5)                   // des define pour une meilleur lisibiliter des couleusr
+#define GREEN (1 << PD6)
+#define BLUE (1 << PD3)
+#define YELLOW (RED | GREEN)
+#define CYAN (GREEN | BLUE)
+#define MAGENTA (BLUE | RED)
+#define WHITE (RED | GREEN | BLUE)
+
+void init_rgb()
+{
+	TCCR0A |= (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00); //on active le fast PWM avec WGM00 et WGM01 a 1 et le Clear Compare match pour COM0A1 et COM0B1 p113
+	TCCR0B |= (1 << CS00); // on choisi No prescaling p116
+
+	TCCR2A |= (1 << COM2B1)| (1 << WGM21) | (1 << WGM20); //on active le fast PWM avec WGM20 et WGM21 a 1 et le Clear Compare match pour COM2B1 p162
+	TCCR2B |= (1 << CS20); // on choisi No prescaling p165
+}
+
+void set_rgb(uint8_t r, uint8_t g, uint8_t b)
+{
+	OCR0B = r; // maj du rapport cyclique de la led p117
+	OCR0A = g; // maj du rapport cyclique de la led p117
+	OCR2B = b; // maj du rapport cyclique de la led p166
+}
+
+void wheel(uint8_t pos)
+{
+	pos = 255 - pos;
+	if (pos < 85)
+	{
+		set_rgb(255 - pos * 3, 0, pos * 3); //transition R vers B
+	}
+	else if (pos < 170)
+	{
+		pos = pos - 85;
+		set_rgb(0, pos * 3, 255 - pos * 3); //transition B vers V
+	}
+	else
+	{
+		pos = pos - 170;
+		set_rgb(pos * 3, 255 - pos * 3, 0); //transition V vers R
+	}
+}
+
 int main(void)
 {
+	DDRB |= LED;
+	DDRD |= LEDRGB;
+	init_rgb();
 
+	ADCSRA |= (1 << ADEN) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // on allume l'ADC avec ADEN p258 et on setup l'horloge de l'ADC p258 et https://marcjuneau.ca/?p=112
+	ADMUX |= (1 << ADLAR) | (1 << REFS0); // ADLAR va nous permettre de "pousser" les 8 plus important bits dans le registre ADCH  p257
+
+	uint8_t i = 0;
+
+	while(1)
+	{
+		// potentiometre
+		ADCSRA |= (1 << ADSC); // on active l'ADC Start Conversion p258
+		while (ADCSRA & (1 << ADSC)){} // on attend la fin de la convertion p258
+		i = ADCH;
+		if (i < 64)
+		{
+			PORTB &= ~(1 << PB0);
+		}
+		else
+		{
+			PORTB |= (1 << PB0);
+		}
+		if (i < 128)
+		{
+			PORTB &= ~(1 << PB1);
+		}
+		else
+		{
+			PORTB |= (1 << PB1);
+		}
+		if (i < 192)
+		{
+			PORTB &= ~(1 << PB2);
+		}
+		else
+		{
+			PORTB |= (1 << PB2);
+		}
+		if (i < 255)
+		{
+			PORTB &= ~(1 << PB4);
+		}
+		else
+		{
+			PORTB |= (1 << PB4);
+		}
+		wheel(i);
+		_delay_ms(20);
+	}
 }
